c0ce778d448278ec1ad6d5f80d532a63ad90487f
Stefan Schnabel <aaron.chen7828@gmail.com>
2025-07-12 18:21:08 -0600

    added homework lmfao

--- ShovelJam2025/HWRoguelike.lua
+++ ShovelJam2025/HWRoguelike.lua
@@ -1,20 +1,21 @@
 function _init()
     poke(0x5F2D, 1)
     makePlayer()
-    make_homework()
+    makeHomework()
     game_state = 0 -- 0 => not active, 1 => active, 2=> shop
 end
 
 
 function _update()
     updateMouse()
+    updateKeyInput()
 end
 
 function _draw()
     cls(1)
     drawHomework() --in order -> background, homework, attention bar, enemies, mouse
     drawMouse()
-    
+    drawKeyInput()
 end
 
 --HELPER
@@ -30,7 +31,8 @@ function makePlayer()
         m_x = 0,
         m_y = 0,
         m_sprite = 1,
-        hw_length = 5
+        hw_length = 5,
+        ans_input = ""
     }
 end
 
@@ -48,11 +50,13 @@ end
 
 
 --HOMEWORK
-function make_homework()
+function makeHomework()
+    hw_complete = false
     hw = {
         problems = {},
         answers = {},
-        index = 0
+        subm_format = {}, -- 0 -> n/a, 3 -> correct, 8 -> wrong
+        index = 1
     }
 
     for i=1, p.hw_length do
@@ -60,7 +64,8 @@ function make_homework()
         add(hw.answers, temp_ans)
         local temp_a = rndb(1,temp_ans)
         local temp_b = temp_ans - temp_a
-        add(hw.problems,""..temp_a.."+"..temp_b.."= ")
+        add(hw.problems,""..temp_a.."+"..temp_b.."=")
+        add(hw.subm_format,0)
     end
 end
 
@@ -69,7 +74,38 @@ function drawHomework()
     rectfill(4,4,58,123,7)
 
     for i=1, #hw.problems do
-        print(hw.problems[i],6,i*8,0)
+        print(hw.problems[i],8,i*8,hw.subm_format[i])
     end
     
+end
+
+function updateKeyInput()
+    local t = stat(31)
+    if (t == "\r") return
+    if (t == "\t") then
+        hw.problems[hw.index] = hw.problems[hw.index]..p.ans_input
+        if (p.ans_input == tostr(hw.answers[hw.index])) then
+            hw.subm_format[hw.index] = 3
+        else
+            hw.subm_format[hw.index] = 8
+        end
+        hw.index += 1
+        p.ans_input = ""
+        return
+    end
+    if (t == "\b") then
+        p.ans_input = sub(p.ans_input,1,#p.ans_input-1)
+        --p.ans_input = p.ans_input.."backspace"
+    else
+        p.ans_input = p.ans_input..t
+    end
+end
+
+function drawKeyInput()
+    if (p.ans_input=="") then
+        --rect(4+#hw.problems[hw.index]*4,hw.index*8,6+#hw.problems[hw.index]*4,hw.index*8 +4,0)
+        spr(2,8+#hw.problems[hw.index]*4,hw.index*8)
+    else
+        print(p.ans_input,8+#hw.problems[hw.index]*4,hw.index*8,0)
+    end
 end
